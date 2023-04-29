018d5015d1ba7673acd268fc4c77d82aa754521d
mohamedSabry0 <mohamedsabry3023@gmail.com>
2023-04-29 23:11:47 +0300

    Add events using provided logic

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,28 +1,58 @@
 import '../styles/calculator.scss';
+import { useState } from 'react';
+import calculate from '../logic/calculate';
 
-const Calculator = () => (
-  <div className="calculator">
-    <div className="result">0</div>
-    <button className="btn gr-bg" type="button">AC</button>
-    <button className="btn gr-bg" type="button">+/-</button>
-    <button className="btn gr-bg" type="button">%</button>
-    <button className="btn or-bg" type="button">÷</button>
-    <button className="btn gr-bg" type="button">7</button>
-    <button className="btn gr-bg" type="button">8</button>
-    <button className="btn gr-bg" type="button">9</button>
-    <button className="btn or-bg" type="button">x</button>
-    <button className="btn gr-bg" type="button">4</button>
-    <button className="btn gr-bg" type="button">5</button>
-    <button className="btn gr-bg" type="button">6</button>
-    <button className="btn or-bg" type="button">-</button>
-    <button className="btn gr-bg" type="button">1</button>
-    <button className="btn gr-bg" type="button">2</button>
-    <button className="btn gr-bg" type="button">3</button>
-    <button className="btn or-bg" type="button">+</button>
-    <button className="btn gr-bg double" type="button">0</button>
-    <button className="btn gr-bg" type="button">.</button>
-    <button className="btn or-bg" type="button">=</button>
-  </div>
-);
+const Calculator = () => {
+  const [calcObj, setCalcObj] = useState({
+    total: '0',
+    next: null,
+    operation: null,
+  });
+
+  const [result, setResult] = useState('0');
+
+  function updateResult(total, next) {
+    if (next) {
+      return next;
+    }
+    if (total) {
+      return total;
+    }
+    return '0';
+  }
+
+  function handleClick(event) {
+    const buttonName = event.target.innerText;
+    const { total, next, operation } = calculate(calcObj, buttonName);
+
+    setCalcObj({ total, next, operation });
+    setResult(updateResult(total, next));
+  }
+
+  return (
+    <div className="calculator">
+      <div className="result">{result}</div>
+      <button onClick={handleClick} className="btn gr-bg" type="button">AC</button>
+      <button onClick={handleClick} className="btn gr-bg" type="button">+/-</button>
+      <button onClick={handleClick} className="btn gr-bg" type="button">%</button>
+      <button onClick={handleClick} className="btn or-bg" type="button">÷</button>
+      <button onClick={handleClick} className="btn gr-bg" type="button">7</button>
+      <button onClick={handleClick} className="btn gr-bg" type="button">8</button>
+      <button onClick={handleClick} className="btn gr-bg" type="button">9</button>
+      <button onClick={handleClick} className="btn or-bg" type="button">x</button>
+      <button onClick={handleClick} className="btn gr-bg" type="button">4</button>
+      <button onClick={handleClick} className="btn gr-bg" type="button">5</button>
+      <button onClick={handleClick} className="btn gr-bg" type="button">6</button>
+      <button onClick={handleClick} className="btn or-bg" type="button">-</button>
+      <button onClick={handleClick} className="btn gr-bg" type="button">1</button>
+      <button onClick={handleClick} className="btn gr-bg" type="button">2</button>
+      <button onClick={handleClick} className="btn gr-bg" type="button">3</button>
+      <button onClick={handleClick} className="btn or-bg" type="button">+</button>
+      <button onClick={handleClick} className="btn gr-bg double" type="button">0</button>
+      <button onClick={handleClick} className="btn gr-bg" type="button">.</button>
+      <button onClick={handleClick} className="btn or-bg" type="button">=</button>
+    </div>
+  );
+};
 
 export default Calculator;
