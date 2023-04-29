82828a2f27437016b1fe95fec6bdd08284d2a2e0
mohamedSabry0 <mohamedsabry3023@gmail.com>
2023-04-30 02:20:38 +0300

    Change hooks according to reviewer feedback

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,42 +1,41 @@
 import { useState, useEffect } from 'react';
 
 const Quote = () => {
-  const [status, setStatus] = useState('idle');
-  const [quote, setQuote] = useState({ quote: '', author: '', error: '' });
+  const [quotes, setQuotes] = useState([]);
+  const [hasError, setHasError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    async function fetchQuote() {
-      setStatus('loading');
+    async function fetchQuotes() {
+      setIsLoading(true);
       try {
-        const response = await fetch('https://api.api-ninjas.com/v1/quotes?category=computers', {
+        const res = await fetch('https://api.api-ninjas.com/v1/quotes?category=computers', {
           headers: {
             'X-Api-Key': '06j/uIU/evT89CS2c1mUvw==dobmC9GDrDgAImpN',
           },
           contentType: 'application/json',
         });
 
-        const data = await response.json();
-        setStatus('succeeded');
-        setQuote({ quote: data[0].quote, author: data[0].author, error: '' });
-        return data;
+        const json = await res.json();
+        setQuotes(json);
       } catch (error) {
-        setStatus('failed');
-        setQuote(...quote, { error });
-        return error;
+        setHasError(true);
       }
+      setIsLoading(false);
     }
-    if (status === 'idle') {
-      fetchQuote();
-    }
-  }, [quote, status]);
+    fetchQuotes();
+  }, [setQuotes, setIsLoading]);
 
-  if (status === 'succeeded') {
-    return (<p>{` today's quote: ${quote.quote} by ${quote.author}` }</p>);
-  }
-  if (status === 'failed') {
-    return (<p>{ quote.error }</p>);
-  }
-  return (<p>Loading...</p>);
+  if (hasError) { return <div>Something went wrong!</div>; }
+
+  if (isLoading) return <div>Loading...</div>;
+  return (
+    <ul>
+      {quotes.map(({ author, quote }) => (
+        <li key={author}>{quote}</li>
+      ))}
+    </ul>
+  );
 };
 
 export default Quote;
